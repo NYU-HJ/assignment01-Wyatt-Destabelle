43572d36170ca4d8ebfba20d008d841d547d0646
Wyatt Destabelle <wyatt@destabelle.net>
2024-02-14 11:04:25 -0500

    3 so few people :(

--- list_todo.h
+++ list_todo.h
@@ -8,32 +8,110 @@ class List: public Container{
       size=0;
     }
     List(int v){
-      // Your code here
+      head = new Node(v);
+      size = 1;
     }
     List(List& l){
-      // Your code here
+      Node * w = l.head;
+      Node * nW = new Node(w->value); 
+      w = w-> next;
+      while(w != NULL)
+      {
+        nW -> next = new Node(w->value);
+        
+      }
+
     }
     ~List(){
-      // Your code here
+      while(head != NULL)
+      {
+        pop();
+        size -= 1;
+      }
+      
     }
-    void add(int v){
-      // Your code here
+    void add(int v)
+    {
+      Node * w = head;
+      if(head ==  NULL)
+      {
+        head = new Node(v);
+        size = 1; 
+        return;
+      }
+      while(w -> next != NULL)
+      {
+        w = w -> next;
+      }
+      w -> next = new Node(v);
     }
     int pop(){
-      // Your code here
-      return 0;
+      Node * w = head;
+      Node * f = NULL;
+      int p;
+      if(w == NULL)
+        return 0;
+      if(w -> next == NULL)
+      {
+        head =  NULL;
+        p = w-> value;
+        free(w);
+        size = 0;
+        return p;
+      }
+      while(w->next!= NULL)
+      {
+        f = w;
+        w = w -> next;
+      }
+      f ->next = NULL;
+      p = w->value;
+      free(w);
+      size -= 1;
+      return p;
+
     }
     bool operator==(const List& rhs){
-      // Your code here
-      return false;
+      Node * r = head;
+      Node * l = rhs.head;
+
+      while(r != NULL && l != NULL)
+      {
+        if(r->value != l->value)
+          return false;
+        r = r -> next;
+        l = l -> next;
+      }
+      if(r == NULL && l == NULL)
+        return true;
+      else
+        return false;
     }
     bool operator!=(const List& rhs){
-      // Your code here
-      return false;
+      Node * r = head;
+      Node * l = rhs.head;
+
+      while(r != NULL && l != NULL)
+      {
+        if(r->value != l->value)
+          return true;
+        r = r -> next;
+        l = l -> next;
+      }
+      if(r == NULL && l == NULL)
+        return false;
+      else
+        return true;
     }
     friend ostream& operator<<(ostream& os, List& l);
 };
 ostream& operator<<(ostream& os, List& l){
-    // Your code here
+    Node * w = l.head;
+    while(w != NULL)
+    {
+      os<<w->value<<",";
+      w = w->next;
+    }
+    os << "Null";
     return os;
 }
